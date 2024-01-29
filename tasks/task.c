370164561dec08b7fa5780657434d9d6da250afc
Alexander Kibeedi <44556559+AKbeedi@users.noreply.github.com>
2024-01-29 12:16:43 +0000

    mini lesson 1

--- tasks/task.c
+++ tasks/task.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h> 
+/**
 int main() {
     int num1, num2;
 
@@ -12,6 +14,24 @@ int main() {
 
     // Calculate and print the sum
     int sum = num1 + num2;
+    printf("Sum: %d\n", sum);
+
+    return 0; // Exit successfully
+}
+*/
+int main(int argc, char *argv[]) {
+    
+    if (argc != 3)
+    {
+        printf("Usage: ./task <num1> <num2>\n");
+        return 0; //return error message if the number of arguements is too many 
+    }
+
+    // Prompt the user for input
+    
+
+    // Calculate and print the sum
+    int sum = (atoi(argv[1])) + (atoi(argv[2]));
     printf("Sum: %d\n", sum);
 
     return 0; // Exit successfully
